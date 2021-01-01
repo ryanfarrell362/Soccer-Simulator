dca3ae0a5b7aa8c12377fd97170ed6438d4df5cb
Ryan Farrell <ryan.farrell362@gmail.com>
2021-01-01 16:54:29 -0500

    Fixed html reading

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const http = require('http');
-fs = require('fs');
-const port = process.env.PORT || 3000
+var http = require('http');
+var fs = require('fs');
+
+const PORT=8080;
 
 fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
+
+    if (err) throw err;
 
     http.createServer(function(request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
         response.write(html);
         response.end();
-    }).listen(8000);
+    }).listen(PORT);
 });
